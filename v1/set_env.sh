3d2d40f90f2e48662cb14924cf24853e022107ce
nadavkal <kal.nadav@gmail.com>
2016-11-17 01:13:06 +0200

    setting the environment

--- v1/set_env.sh
+++ v1/set_env.sh
@@ -1,1 +1,9 @@
 apt install python-tk
+apt install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
+easy_install greenlet
+easy_install gevent
+apt install gcc
+apt install python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp27-none-linux_x86_64.whl
+pip install $TF_BINARY_URL
+pip install -r requirements.txt
